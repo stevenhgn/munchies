306e04857fba9f203fea0ae6ef20260214b57f33
Alex Nguyen <hoanghai92@hotmail.com>
2020-04-12 18:06:27 +0200

    Add styledcomponents and materiaul ui

---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Card = () => {
+    return <Wrapper>Home</Wrapper>
+}
+
+const Wrapper = styled.div`
+    background-color: aquamarine;
+`
+
+export default Card;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Card from '../../components/Card/Card';
+
+const Home = () => {
+    return <div><h1>Home</h1>
+    <Card/></div>
+}
+
+export default Home;
