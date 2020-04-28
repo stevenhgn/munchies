f92203209c0586a4af180cf2b1ffa8e1c561a5fa
shnguyen <shnguyen@student.matnat.uio.no>
2020-04-28 20:30:06 +0200

    Restructured previous files into a webapp folder and made new rest_api folder to test api

---
+++ webapp/webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: ["react-hot-loader/patch", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: require("html-webpack-template"),
+      inject: false,
+      appMountId: "app",
+      filename: "index.html",
+    }),
+  ],
+  node: {
+    fs: "empty",
+    net: "empty",
+  },
+  // target: "node"
+};
+
+module.exports = config;
