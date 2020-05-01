8d5ecd8df121d836ce689382703662abccd36f15
shnguyen <shnguyen@student.matnat.uio.no>
2020-05-02 01:35:05 +0200

    Removed unused comment

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// let express = require("express");
-// let app = express();
 
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "path/to/your/index.html"), function (err) {
-//     if (err) {
-//       res.status(500).send(err);
-//     }
-//   });
-// });
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App name="Jane" />, mountNode);
