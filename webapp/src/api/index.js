ba4f50551d52ef62e91f6927f0621de4c0b554b7
shnguyen <shnguyen@student.matnat.uio.no>
2020-05-09 01:05:25 +0200

    Webapp: Change backendapi uri

--- webapp/src/api/index.js
+++ webapp/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://warm-reef-84103.herokuapp.com/api",
   timeout: 1000,
 });
 export default instance;
